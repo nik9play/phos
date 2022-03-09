223ebaf651576606e995a4aabd84de5919586377
nik9 <n-bogun@mail.ru>
2022-03-10 01:17:14 +0300

    add scroll animation

---
+++ fos/Controls/ScrollViewerEx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace fos
+{
+    internal class ScrollViewerEx : ScrollViewer
+    {
+        protected override void OnInitialized(EventArgs e)
+        {
+            base.OnInitialized(e);
+
+            if (Style == null && ReadLocalValue(StyleProperty) == DependencyProperty.UnsetValue)
+            {
+                SetResourceReference(StyleProperty, typeof(ScrollViewer));
+            }
+        }
+
+        protected override void OnMouseWheel(MouseWheelEventArgs e)
+        {
+            if (e.Handled) { return; }
+            ScrollViewerHelperEx.OnMouseWheel(this, e);
+            e.Handled = true;
+        }
+    }
+}
