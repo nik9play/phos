8cfc74bc3f1f212963bb815671e411584bde97ec
nik9 <n-bogun@mail.ru>
2022-04-17 20:29:13 +0300

    the slider now enters drag mode after moving thumb to a point

--- fos/Tools/SliderTools.cs
+++ fos/Tools/SliderTools.cs
@@ -6,26 +6,6 @@ namespace fos;
 
 public class SliderTools : DependencyObject
 {
-    public static readonly DependencyProperty MoveToPointOnDragProperty = DependencyProperty.RegisterAttached(
-        "MoveToPointOnDrag", typeof(bool), typeof(SliderTools), new PropertyMetadata
-        {
-            PropertyChangedCallback = (obj, changeEvent) =>
-            {
-                var slider = (Slider)obj;
-                if ((bool)changeEvent.NewValue)
-                    slider.MouseMove += (obj2, mouseEvent) =>
-                    {
-                        if (mouseEvent.LeftButton == MouseButtonState.Pressed)
-                            slider.RaiseEvent(new MouseButtonEventArgs(mouseEvent.MouseDevice, mouseEvent.Timestamp,
-                                MouseButton.Left)
-                            {
-                                RoutedEvent = UIElement.PreviewMouseLeftButtonDownEvent,
-                                Source = mouseEvent.Source
-                            });
-                    };
-            }
-        });
-
     public static bool GetMoveToPointOnDrag(DependencyObject obj)
     {
         return (bool)obj.GetValue(MoveToPointOnDragProperty);
@@ -35,4 +15,40 @@ public class SliderTools : DependencyObject
     {
         obj.SetValue(MoveToPointOnDragProperty, value);
     }
+
+    public static readonly DependencyProperty MoveToPointOnDragProperty = DependencyProperty.RegisterAttached(
+        "MoveToPointOnDrag", typeof(bool), typeof(SliderTools), new PropertyMetadata
+        {
+            PropertyChangedCallback = (obj, changeEvent) =>
+            {
+                var slider = (Slider)obj;
+                if ((bool)changeEvent.NewValue)
+                {
+                    slider.MouseMove += (sender, args) =>
+                    {
+                        if (args.LeftButton == MouseButtonState.Pressed && clickedInSlider)
+                        {
+                            var thumb = (slider.Template.FindName("PART_Track", slider) as System.Windows.Controls.Primitives.Track).Thumb;
+                            thumb.RaiseEvent(new MouseButtonEventArgs(args.MouseDevice, args.Timestamp, MouseButton.Left)
+                            {
+                                RoutedEvent = UIElement.MouseLeftButtonDownEvent,
+                                Source = args.Source
+                            });
+                        }
+                    };
+
+                    slider.AddHandler(UIElement.PreviewMouseLeftButtonDownEvent, new RoutedEventHandler((sender, args) =>
+                    {
+                        clickedInSlider = true;
+                    }), true);
+
+                    slider.AddHandler(UIElement.PreviewMouseLeftButtonUpEvent, new RoutedEventHandler((sender, args) =>
+                    {
+                        clickedInSlider = false;
+                    }), true);
+                }
+            }
+        });
+
+    private static bool clickedInSlider;
 }
