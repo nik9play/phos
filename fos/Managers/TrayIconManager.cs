8838a7a0e46ed804213ef8d064991d654ad6128c
nik9 <n-bogun@mail.ru>
2022-04-22 01:20:02 +0300

    rewrite tray icon managment and add mouse wheel functionality

--- fos/Managers/TrayIconManager.cs
+++ fos/Managers/TrayIconManager.cs
@@ -1,10 +1,16 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
+using DebounceThrottle;
 using fos.Properties;
 using fos.Tools;
-using Hardcodet.Wpf.TaskbarNotification;
+using fos.ViewModels;
+using fos.Win32;
+using H.Hooks;
+using H.NotifyIcon;
+using H.NotifyIcon.Core;
 using ModernWpf;
 using ModernWpf.Controls;
 using Size = System.Drawing.Size;
@@ -13,7 +19,12 @@ namespace fos;
 
 public static class TrayIconManager
 {
-    private static TaskbarIcon TrayIcon { get; set; }
+    private static readonly ThrottleDispatcher ThrottleDispatcher =
+        new(100);
+
+    private static TaskbarIcon IconInTray { get; set; }
+    private static Guid TrayIconGuid { get; set; }
+    private static LowLevelMouseHook MouseHook { get; set; }
 
     public static void InitTrayIcon()
     {
@@ -38,13 +49,97 @@ public static class TrayIconManager
 
         var taskbarIconToolTip = new ToolTip { Content = "phos" };
 
-        TrayIcon = new TaskbarIcon
+        IconInTray = new TaskbarIcon
         {
             TrayToolTip = taskbarIconToolTip,
+            ToolTipText = "phos",
             ContextMenu = taskbarIconContextMenu,
             LeftClickCommand = CommonCommands.TogglePopupCommand,
             NoLeftClickDelay = true
         };
+
+        TrayIconGuid = TrayIcon.CreateUniqueGuidForEntryAssembly();
+
+        MouseHook = new LowLevelMouseHook();
+
+        IconInTray.ForceCreate();
+
+        MouseHook.Wheel += OnMouseWheel;
+
+        MouseHook.Start();
+    }
+
+    private static void OnMouseWheel(object sender, MouseEventArgs args)
+    {
+        ThrottleDispatcher.Throttle(() =>
+        {
+            var trayIconRectangle = GetRectangle();
+
+            if (trayIconRectangle.HasValue &&
+                args.Position.X > trayIconRectangle?.X &&
+                args.Position.X < trayIconRectangle.Value.X + trayIconRectangle.Value.Width &&
+                args.Position.Y > trayIconRectangle.Value.Y &&
+                args.Position.Y < trayIconRectangle.Value.Y + trayIconRectangle.Value.Height)
+                Application.Current.Dispatcher.Invoke(delegate
+                {
+                    var offset = args.Delta > 0 ? 5 : -5;
+
+                    try
+                    {
+                        if (SettingsController.Store.AllMonitorsModeEnabled)
+                        {
+                            var newBrightness = MainWindowViewModel.AllMonitorsBrightness + offset;
+
+                            if (newBrightness < 0)
+                                newBrightness = 0;
+
+                            if (newBrightness > 100)
+                                newBrightness = 100;
+
+                            MainWindowViewModel.AllMonitorsBrightness = (uint)newBrightness;
+                        }
+                        else
+                        {
+                            foreach (var el in MainWindowViewModel.Monitors)
+                            {
+                                var newBrightness = el.Brightness + offset;
+
+                                if (newBrightness < 0)
+                                    newBrightness = 0;
+
+                                if (newBrightness > 100)
+                                    newBrightness = 100;
+
+                                el.Brightness = (uint)newBrightness;
+                            }
+                        }
+                    }
+                    catch
+                    {
+                        // ignored
+                    }
+                });
+        });
+    }
+
+    public static Rect? GetRectangle()
+    {
+        try
+        {
+            var rect = Shell32.GetNotifyIconRect(TrayIconGuid);
+
+            return new Rect
+            {
+                X = rect.Left,
+                Y = rect.Top,
+                Width = rect.Right - rect.Left,
+                Height = rect.Bottom - rect.Top
+            };
+        }
+        catch
+        {
+            return null;
+        }
     }
 
     public static void UpdateTheme(ApplicationTheme theme)
@@ -53,7 +148,7 @@ public static class TrayIconManager
         var iconSize = new Size((int)(SystemParameters.SmallIconWidth * factor),
             (int)(SystemParameters.SmallIconHeight * factor));
 
-        TrayIcon.Icon = new Icon(theme == ApplicationTheme.Light ? Resources.iconBlack : Resources.iconWhite,
+        IconInTray.Icon = new Icon(theme == ApplicationTheme.Light ? Resources.iconBlack : Resources.iconWhite,
             iconSize);
     }
 }
