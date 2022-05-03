ff25bf61a39609ce5d8550fe0aefea9009022f98
nik9 <n-bogun@mail.ru>
2022-05-04 00:33:34 +0300

    package helper

---
+++ fos/Tools/PackageHelper.cs
@@ -0,0 +1,64 @@
+﻿using System.Diagnostics;
+using System.Text;
+using Windows.ApplicationModel;
+using fos.Win32Interops;
+
+namespace fos.Tools;
+
+public class PackageHelper
+{
+    private const long APPMODEL_ERROR_NO_PACKAGE = 15700L;
+
+    private static bool? hasIdentity;
+
+    public static bool HasIdentity()
+    {
+        if (hasIdentity == null)
+        {
+            int length = 0;
+            var sb = new StringBuilder(0);
+            Kernel32.GetCurrentPackageFullName(ref length, sb);
+
+            sb = new StringBuilder(length);
+            int error = Kernel32.GetCurrentPackageFullName(ref length, sb);
+
+            hasIdentity = error != APPMODEL_ERROR_NO_PACKAGE;
+        }
+
+        return hasIdentity.Value;
+    }
+
+    private static bool? isContainerized;
+
+    public static bool IsContainerized()
+    {
+        if (isContainerized == null)
+        {
+            // If MSIX or sparse
+            if (HasIdentity())
+            {
+                // Sparse is identified if EXE is running outside of installed package location
+                var packageInstalledLocation = Package.Current.InstalledLocation.Path;
+                var actualExeFullPath = Process.GetCurrentProcess().MainModule.FileName;
+
+                // If inside package location
+                if (actualExeFullPath.StartsWith(packageInstalledLocation))
+                {
+                    isContainerized = true;
+                }
+                else
+                {
+                    isContainerized = false;
+                }
+            }
+
+            // Plain Win32
+            else
+            {
+                isContainerized = false;
+            }
+        }
+
+        return isContainerized.Value;
+    }
+}
