a2f75739718f948ecd7dd9509f0206f8b49659ca
nik9 <n-bogun@mail.ru>
2022-05-10 23:32:05 +0300

    add scheduler dialog

---
+++ fos/Dialogs/SchedulerDialog.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using fos.ViewModels;
+using ModernWpf.Controls;
+
+namespace fos.Dialogs
+{
+    /// <summary>
+    /// Логика взаимодействия для SchedulerDialog.xaml
+    /// </summary>
+    public partial class SchedulerDialog : ContentDialog
+    {
+        public BrightnessSchedulerSettingsElement BrightnessTask { get; set; }
+
+        public SchedulerDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void AddClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+            //PageBrightnessSchedulerViewModel.BrightnessTasks.Add(new BrightnessSchedulerSettingsElement()
+            //{
+            //    AllMonitors = false,
+            //    DeviceId = "123",
+            //    Brightness = 10,
+            //    Time = new TimeSpan(1, 1, 1),
+            //    Name = "12333"
+            //});
+        }
+    }
+}
