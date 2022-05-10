3a3e4b3b88fe9a7ec97d3b10bfb133179fcda836
nik9 <n-bogun@mail.ru>
2022-05-10 23:32:33 +0300

    add brightness scheduler page

---
+++ fos/Converters/TimespanConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace fos;
+
+public class TimespanConverter : JsonConverter<TimeSpan>
+{
+    public const string TimeSpanFormatString = @"hh\:mm";
+
+    public override void WriteJson(JsonWriter writer, TimeSpan value, JsonSerializer serializer)
+    {
+        var timespanFormatted = $"{value.ToString(TimeSpanFormatString)}";
+        writer.WriteValue(timespanFormatted);
+    }
+
+    public override TimeSpan ReadJson(JsonReader reader, Type objectType, TimeSpan existingValue, bool hasExistingValue, JsonSerializer serializer)
+    {
+        TimeSpan parsedTimeSpan;
+        TimeSpan.TryParseExact((string)reader.Value, TimeSpanFormatString, null, out parsedTimeSpan);
+        return parsedTimeSpan;
+    }
+}
---
+++ fos/SettingsPages/BrightnessScheduler.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Windows;
+using Windows.ApplicationModel;
+using fos.Tools;
+using fos.ViewModels;
+using Microsoft.Toolkit.Uwp.Notifications;
+
+namespace fos.SettingsPages;
+
+public partial class BrightnessScheduler : ModernWpf.Controls.Page
+{
+    public BrightnessScheduler()
+    {
+        InitializeComponent();
+        DataContext = new PageBrightnessSchedulerViewModel();
+    }
+
+    private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+    {
+        new ToastContentBuilder()
+                .AddArgument("action", "test")
+                .AddText("dfasdasdf")
+                .Show();
+    }
+}
